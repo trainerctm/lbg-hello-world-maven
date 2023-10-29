f2eb6e4bb1efa1d7ff1b878d1d5d5b00dafdb662
trainerctm <144346504+trainerctm@users.noreply.github.com>
2023-10-29 12:57:37 +0000

    Update App.java

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Courage"));
+        System.out.println(sayHelloToSomeone("Trainer"));
         System.out.println(sayGoodbye());
     }
 
